858cdca6a74ca347e2227f343e1b72b33e42fad3
IsmailBayramov <geri60884@gmail.com>
2024-07-08 03:18:14 +0500

    added new const - tableName

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const fs = require("fs");
 const pg = require("pg");
 const axios = require("axios");
 
+const tableName = "IsmailBayramov";
+
 const config = {
     connectionString:
         "postgres://candidate:62I8anq3cFq5GYh2u4Lh@rc1b-r21uoagjy1t7k77h.mdb.yandexcloud.net:6432/db1",
@@ -18,7 +20,7 @@ const conn = new pg.Client(config);
 
 async function createTable() {
     const createQuery = `
-        CREATE TABLE IF NOT EXISTS IsmailBayramov (
+        CREATE TABLE IF NOT EXISTS ${tableName} (
             id SERIAL PRIMARY KEY,
             name TEXT NOT NULL,
             data JSONB
@@ -29,7 +31,7 @@ async function createTable() {
 
 async function insertCharacter(character) {
     const insertQuery = `
-        INSERT INTO IsmailBayramov (
+        INSERT INTO ${tableName} (
             name, data
         ) VALUES (
             $1, $2
